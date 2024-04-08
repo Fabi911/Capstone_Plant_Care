2d3086a16db1d2ea65a574186a496a9126ba1581
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-04-08 12:24:29 +0200

    fonts

--- styles.js
+++ styles.js
@@ -1,6 +1,41 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
+// Define global styles
 export default createGlobalStyle`
+:root {
+  // Main color scheme
+  --bg-color: #004643;
+  --second-bg-color: #abd1c6;
+  --text-color: #e8e4e6;
+  --main-color: #2de2e6;
+  --headline-color: "#fffffe";
+  --paragraph-color: #abd1c6;
+  --highlight-color: #f9bc60;
+  --tertiary-color: #e16162;
+  --stroke-color: #001e1d;
+  --button-color: #f9bc60;
+  --div-color: #abd1c6;
+  // Alternative color scheme
+  --bg2-color: #e3f6f5;
+  --second-bg2-color: #abd1c6,
+  --text2-color: #e8e4e6;
+  --main2-color: #2de2e6;
+  --headline2-color: #272343;
+  --subHeadline-color: #2d334a;
+  --card-color: #fffffe;
+  --paragraph2-color: #abd1c6;
+  --highlight2-color: #ffd803;
+  --tertiary2-color: #bae8e8;
+  --stroke2-color: #272343;
+  --button2-color: #f9bc60;
+  // Synthwave Colors
+  --bg3-color: #241B2F;
+  --text3-color: #72f1b8;
+  --main3-color: #fff5f6;
+  --highlight2-color: #f92aad;
+  --tertiary3-color: #f97e72;
+
+}
   *,
   *::before,
   *::after {
@@ -11,12 +46,24 @@ export default createGlobalStyle`
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-color: #004643;
-    text-align: center;
-    gap: 1.5rem;
-    color: white;
+    background-color: var(--bg3-color); 
+    color: var(--text3-color);
     margin: 0;
     font-family: system-ui;
     margin-bottom: 80px;
+    text-align: center;
+  }
+`;
+
+// Styled component for Container
+export const Container = styled.div`
+  background: var(--second-bg-color);
+  color: var(--text-color);
+  width: 80vw;
+  height: auto;
+  border: 1px solid black;
+  border-radius: 20px;
+  &:hover {
+    box-shadow: 5px 5px 5px #f9bc60;
   }
 `;
