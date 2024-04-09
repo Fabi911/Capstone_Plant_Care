09c63fbdb66dc1b0e8abb315dd1f10a31cdd36cb
Fabian Döz <fabian@doez.info>
2024-04-09 16:54:23 +0200

    Update overview.js

--- pages/overview.js
+++ pages/overview.js
@@ -18,7 +18,7 @@ export default function Overview({ plants, handleToggleOwnedPlants }) {
      <BackArrow link="/" />
 
    
-
+<AddPlantLink />
       <PlantsList
         plants={plants}
         handleToggleOwnedPlants={handleToggleOwnedPlants}
