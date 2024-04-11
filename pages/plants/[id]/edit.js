6ecff2c7ee34fa0f04d731922347ee10468a4a29
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-04-11 11:12:44 +0200

    solved

--- pages/plants/[id]/edit.js
+++ pages/plants/[id]/edit.js
@@ -23,6 +23,7 @@ export default function EditPage({ plants, handleEditPlant }) {
         onSubmit={handleSubmit}
         defaultData={editPlant}
         formName={"editPlant"}
+        isEditMode={true}
       />
     </>
   );
