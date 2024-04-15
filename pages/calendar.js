c816a91ed68234bbcf44e90a70011b0054f171c9
Henrik <henrik@bischkopf.de>
2024-04-15 16:17:46 +0200

    pagination and tested mockData

--- pages/calendar.js
+++ pages/calendar.js
@@ -2,7 +2,33 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
+const mockData = {
+  _id: { $oid: "66168f95531a604507133c64" },
+  name: "Snake Plant",
+  botanical_name: "Sansevieria trifasciata",
+  water_need: "Low",
+  fertiliser_season: ["Spring", "Summer"],
+  isOwned: false,
+  gallery: [],
+  image:
+    "https://images.unsplash.com/photo-1593482892290-f54927ae1bb6?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8U25ha2UlMjBQbGFudHxlbnwwfHwwfHx8MA%3D%3D",
+  notes: ["Gute alte Snake", "läuft", "läuft immernoch"],
+  start: new Date("2024-04-15"),
+  /* new Date().toISOString() */
+};
+
 export default function Calendar() {
+  const renderEventContent = (eventInfo) => {
+    return (
+      <div>
+        <b>{eventInfo.event.title}</b> {/* Display event title */}
+        <p>{eventInfo.event.extendedProps.botanical_name}</p>{" "}
+        {/* Display botanical name */}
+        <p>Water Need: {eventInfo.event.extendedProps.water_need}</p>{" "}
+        {/* Display water need */}
+      </div>
+    );
+  };
   return (
     <>
       <FullCalendar
@@ -23,8 +49,9 @@ export default function Calendar() {
           },
         }}
         editable={true}
-        /* events={allMeals} */
+        events={[mockData]}
         eventOrder={"category"}
+        eventContent={renderEventContent}
         /* eventClick={handleEventClick} */
       />
     </>
