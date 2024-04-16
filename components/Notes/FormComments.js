6e68d08ce5141020438a56b08377efe1476da5e8
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-04-16 15:23:17 +0200

    done

--- components/Notes/FormComments.js
+++ components/Notes/FormComments.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 export default function FormComments({ onSubmit, plant, mutate }) {
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -14,17 +16,45 @@ export default function FormComments({ onSubmit, plant, mutate }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <FormContainer onSubmit={handleSubmit}>
       <label htmlFor="notes">Add a comment:</label>
-      <input
+      <textarea
         type="text"
         id="notes"
         name="notes"
-        rows="2"
-        cols="50"
+        rows="3"
+        cols="30"
         required
-      ></input>
-      <button type="submit">Post</button>
-    </form>
+        maxLength={200}
+        minLength={1}
+      ></textarea>
+      <ButtonSend type="submit">💐</ButtonSend>
+    </FormContainer>
   );
 }
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  border: 1px solid #404040;
+  padding: 10px 20px;
+  border-radius: 5px;
+  box-shadow: var(--box-shadow-default);
+  margin: 10px 0px;
+`;
+
+const ButtonSend = styled.button`
+  background: rgba(255, 255, 255, 0.6);
+  cursor: pointer;
+  border: none;
+  box-shadow: var(--box-shadow-default);
+  font-size: 1rem;
+  border-radius: 50%;
+  width: 2rem;
+  height: 2rem;
+  &:active {
+    box-shadow: inset var(--box-shadow-default);
+  }
+`;
