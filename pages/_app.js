024d0ff29560a2abf782b3e77ef841b306de90c3
Fabian Döz <fabian@doez.info>
2024-04-16 10:35:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 
 import { mutate, SWRConfig } from "swr";
 
-import { SWRConfig } from "swr";
+
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
