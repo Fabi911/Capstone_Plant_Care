d8a1aa16d2cea8df4978ba9bc45faf656ee3a5e1
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-04-18 09:34:12 +0200

    styled

--- components/Notes/EditNoteForm.js
+++ components/Notes/EditNoteForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 export default function EditNoteForm({ editedNote, onSave, onCancel }) {
   const [editedText, setEditedText] = useState(editedNote);
@@ -10,18 +11,41 @@ export default function EditNoteForm({ editedNote, onSave, onCancel }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <textarea
+      <input
         value={editedText}
         onChange={(event) => setEditedText(event.target.value)}
         rows={3}
         cols={30}
       />
       <div>
-        <button type="submit">Save</button>
-        <button type="button" onClick={onCancel}>
+        <ButtonSubmit type="submit">Save</ButtonSubmit>
+        <ButtonCancel type="button" onClick={onCancel}>
           Cancel
-        </button>
+        </ButtonCancel>
       </div>
     </form>
   );
 }
+
+const ButtonSubmit = styled.button`
+  background: rgba(255, 255, 255, 0.6);
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  box-shadow: var(--box-shadow-default);
+  margin-right: 10px;
+  &:active {
+    box-shadow: inset var(--box-shadow-default);
+  }
+`;
+
+const ButtonCancel = styled.button`
+  background: rgba(255, 255, 255, 0.6);
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  box-shadow: var(--box-shadow-default);
+  &:active {
+    box-shadow: inset var(--box-shadow-default);
+  }
+`;
