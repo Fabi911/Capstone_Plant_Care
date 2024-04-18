fb638ac18a82cf480160356d067f2f9a42a9caf0
Fabian Döz <fabian@doez.info>
2024-04-18 17:24:15 +0200

    finsih Login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,33 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === "stranger_boy" &&
+          credentials.password === "stranger_plant"
+        ) {
+          return {
+            name: "Stranger Boy",
+            email: "stranger@Plants.com",
+          };
+        } else {
+          return null;
+        }
+      },
     }),
     // DiscordProvider({
     //   clientId: process.env.DISCORD_CLIENT_ID,
