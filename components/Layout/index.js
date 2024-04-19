7ff7a782fc7ca80b1a1bceac6cb56e068d76499f
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-04-19 10:38:57 +0200

    fix

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,16 +1,24 @@
 import { Header, Footer } from "./StyledLayout";
 import Logo from "./Logo";
 import Weather from "../Weather/Weather";
+import styled from "styled-components";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header>
         <Logo />
-        <Weather />
+        <WeatherBox>
+          <Weather />
+        </WeatherBox>
       </Header>
       {children}
       <Footer>&copy; Copyright</Footer>
     </>
   );
 }
+
+const WeatherBox = styled.div`
+  position: absolute;
+  left: 45px;
+`;
